80eff367073694d986d3fea7777ed8dd25e22422
Naman Patel <46571853+np453@users.noreply.github.com>
2021-02-18 11:28:10 +0530

    minor

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+// const helmet = require('helmet')
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const path = require('path');
+
+
+//Port
+const PORT = 5151;
+
+//.env file
+dotenv.config();
+
+
+//Middlewaress
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
+
+
+
+//connect to DB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log("Database is connected!"));
+
+app.use(express.static('client/build'));
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+
+
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.listen(PORT, function() {
+    console.log(`App running on port ${PORT}`);
+}); 
+ 
